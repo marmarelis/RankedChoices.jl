561fc716c049a3586d0c71abc5ef2e618394f3df
Myrl <myrl@marmarel.is>
2021-12-29 13:04:37 -0800

    fix project issues

--- src/RankedChoices.jl
+++ src/RankedChoices.jl
@@ -12,7 +12,7 @@
 ##  See the License for the specific language governing permissions and
 ##  limitations under the License.
 
-module OptimalVoting
+module RankedChoices
 
 include("structures.jl")
 include("sampler.jl")
